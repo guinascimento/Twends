0c4fdcc55419cb102670399ccaa5cdd04fbcc84d
Guilherme Nascimento <guilherme.ruby@gmail.com>
2009-12-01 11:09:36 -0200

    refactoring on REST methods

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -5,7 +5,7 @@ class TrendsController < ApplicationController
 		minute = Twitter::Trends.current
 		@photos = {}
 		total = 0
-		raise ArgumentError
+
 		for trend in minute do
 			total += 1
 			@photos[trend.name] = Flickr.search(trend.name)
