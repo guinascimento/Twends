37452e21fbd024099b6ef4d0c02e608f175ef8be
Guilherme Nascimento <guilherme.ruby@gmail.com>
2009-12-01 10:51:50 -0200

    refactoring on REST methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def is_active?(action_name)
+		"current" if params[:action] == action_name
+	end
 end
