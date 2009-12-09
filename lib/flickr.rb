a5ae6b7e908ff73fd13a97c8c23c19c89acdae9b
Guilherme Nascimento <guilherme.ruby@gmail.com>
2009-12-09 01:17:20 -0200

    adding flickr.rb

---
+++ lib/flickr.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'hpricot'
+
+class Flickr
+	def self.search(text, per_page = 1)
+		text = clean(text)
+		url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=da0e80ad7b47166b7fc008a1e4ac604f&per_page=#{per_page}&tags=#{text}&tagmode=any"
+		xml = Net::HTTP.get(URI.parse(url));
+
+		doc = Hpricot.XML(xml)
+		throw "An exception occurred while getting the photos from Flickr." unless doc.at("rsp")["stat"] == "ok"
+
+		if doc.at("photos")["total"].to_i > 0
+			(doc/:photos/:photo).collect do |p|
+				Photo.new(p)
+			end
+		end
+	end
+
+	def self.clean(text)
+		text = text.gsub(/[#)...é’]/, "")
+		text = text.gsub(/[ ]/, ",")
+	end
+end
